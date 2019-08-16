73c8dc9f8111a30a3892c9953db6639adee8633c
Shivam Maheshwari <39937119+Maheshwari2604@users.noreply.github.com>
2019-08-16 04:44:12 +0000

    Create greedy.py

---
+++ greedy.py
@@ -0,0 +1,23 @@
+#Greedy method
+
+#This method is used for optimization problem
+
+#It is used for the result if solution demands the minimum cost or maximum cost
+#low cost more value(minimum cost)
+#there can be more than one result but every solution has only one feasible solution
+
+#####-----Strategies used for optimization problem-----######
+#----------------Greedy Method
+#----------------Dynamic Programming
+#----------------Branch and Bound
+
+#####-----GREEDY MEHTOD-----#####
+# Algo Greedy(a,n):
+#     for i in n:
+#         x = select(a)
+#         if feasible(x) then:
+#             sol = x
+
+#####-----JOB SEQUENCING PROBLEM-----######
+#kii job complte ho in less time and more profit
+
